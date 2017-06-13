2690c390f02c9543101f5a041b8272259e401ec1
Panu Lahtinen <pnuu+git@iki.fi>
2017-06-13 08:15:02 +0300

    Add logging, use self.config, add cloud masking

--- fffsat/forest_fire.py
+++ fffsat/forest_fire.py
@@ -7,6 +7,8 @@
 #
 #   Panu Lahtinen <panu.lahtinen@fmi.fi>
 
+import logging
+
 import numpy as np
 
 from fffsat import utils
@@ -16,6 +18,8 @@ class ForestFire(object):
 
     """Class for creating forest fire hot spots based on algorithm by
     Planck et. al."""
+
+    logger = logging.get_logger("ForestFire")
 
     def __init__(self, config):
         # Configuration dictionary for ForestFire class
@@ -27,20 +31,25 @@ class ForestFire(object):
         # All invalid pixels are set as True.  After processing the locations
         # marked as False are the valid forest fires.
         self.mask = None
-        # Incoming message
-        self.msg = None
         # Probability of fire
         self.probability = None
         # Quality of the hot spot retrieval
         self.quality = None
+        # Cloud mask
+        self.cloud_mask = None
 
     def run(self, msg=None, sat_fname=None, cma_fname=None):
         """Run everything"""
         if msg is not None:
-            self.msg = msg
             sat_fname, cma_fname = utils.get_filenames_from_msg(msg)
+        if sat_fname is None:
+            self.logger.critical("No satellite data in message")
+            return False
         self.data = utils.read_sat_data(sat_fname,
                                         self.config["channels_to_load"])
+        if cma_fname is not None:
+            self.cloud_mask = utils.read_cma(cma_fname)
+
         self.mask = self.data[self.config["NIR_CHAN_NAME"]].mask.copy()
 
         self.mask_data()
@@ -53,9 +62,9 @@ class ForestFire(object):
         """Cleanup after processing."""
         self.data = None
         self.mask = None
-        self.msg = None
         self.probability = None
         self.quality = None
+        self.cloud_mask = None
 
     def apply_mask(self, mask):
         """Apply given mask to the product mask"""
@@ -75,57 +84,78 @@ class ForestFire(object):
 
     # Static masks read from a file and resampled to match the swath
 
-    def land_cover_mask(self, config):
+    def land_cover_mask(self):
         """Read and resample land cover exclusion mask"""
-        mask, lons, lats = utils.read_land_cover(config["mask_file"])
-        mask = self.resample_aux(mask, lons, lats)
-        return mask
-
-    def snow_mask(self, config):
-        """Read and resample snow exclusion mask"""
-        mask, lons, lats = utils.read_snow_mask(config["mask_file"])
+        mask, lons, lats = \
+            utils.read_land_cover(self.config["land_cover_mask"]["mask_file"])
         mask = self.resample_aux(mask, lons, lats)
         return mask
 
     # Masking based on data in satellite projection, either external
     # or calculated from reflectances/BTs
 
-    def cloud_mask(self, config):
-        """Read cloud exclusion mask"""
-        # Get cloud mask (NWC SAF CMa) filename from the message
-        fname = None  # msg[""]
-        mask = utils.read_cloud_mask(fname)
+    def snow_mask(self, config):
+        """Read and resample snow exclusion mask"""
+        # Read from NWC SAF?
+        mask, lons, lats = utils.read_snow_mask(config["mask_file"])
+        mask = self.resample_aux(mask, lons, lats)
         return mask
 
-    def water_mask(self, config):
+    def cloud_mask(self):
+        """Get exclusion mask"""
+        if self.cloud_mask is not None:
+            return self.cloud_mask
+        else:
+            self.logger.warning("NWC SAF cloud mask not available")
+            return self.create_cloud_mask()
+
+    def water_mask(self):
         """Create water mask"""
         # eq. 1 from Planck et. al.
         # maybe not use this?
         mask = None
         return mask
 
-    def sun_glint_mask(self, config):
+    def sun_glint_mask(self):
         """Create Sun glint mask"""
         # eq. 5 - 8 from Planck et. al.
         mask = None
         return mask
 
-    def fcv_mask(self, config):
+    def fcv_mask(self):
         """Calculate fraction of vegetation exclusion mask"""
-        # eq. 9 and 10 from Planck et. al
-        ch1 = self.data[self.config["VIS_CHAN_NAME"]]
-        ch2 = self.data[self.config["NIR_CHAN_NAME"]]
+        # eq. 9 and 10 from Planck et.al. 2017
+        ch1 = self.data[self.config["vis_chan_name"]]
+        ch2 = self.data[self.config["nir_chan_name"]]
         ndvi = (ch2 - ch1) / (ch2 + ch1)
         ndvi_min = np.min(ndvi)
         ndvi_max = np.max(ndvi)
         fvc = ((ndvi - ndvi_min) / (ndvi_max - ndvi_min)) ** 2
-        mask = fvc < config["fvc_threshold"]
+        mask = fvc < self.config["fcv_mask"]["threshold"]
         return mask
 
     def swath_edge_mask():
         """Create mask for the swath edges"""
         mask = None
         return mask
+
+    def create_cloud_mask(self):
+        """Create cloud mask from satellite data."""
+        # Planck et.al. 2017 eq. 2 - 4
+        vis = self.data[self.config["vis_chan_name"]]
+        nir = self.data[self.config["nir_chan_name"]]
+        mir = self.data[self.config["mir_chan_name"]]
+        ir1 = self.data[self.config["ir1_chan_name"]]
+        ir2 = self.data[self.config["ir2_chan_name"]]
+
+        w_mir = np.exp((310. - mir) / 20.)
+        w_delta_ch = np.exp((mir - (ir1 + ir2) / 2. - 14.) / 14.)
+        mean_vis_nir = (vis + nir) / 2.
+
+        cloud_th = self.config["cloud_mask"]["threshold"]
+        clouds = (w_mir * w_delta_ch * mean_vis_nir) > cloud_th
+
+        return clouds
 
 
 def select_candidates(prob_level):
