6c757f9602cf1788b389a50f68846f2bcbae07ad
Panu Lahtinen <pnuu+git@iki.fi>
2017-07-04 08:35:25 +0300

    Fix western limit, add cropping

--- bin/prepare_globcover.py
+++ bin/prepare_globcover.py
@@ -15,10 +15,15 @@ GLOBCOVER_URL = "http://due.esrin.esa.int/files/Globcover2009_V2.3_Global_.zip"
 GLOBCOVER_RESOLUTION = 1. / 360.
 GLOBCOVER_NORTH_LAT_LIMIT = 90.
 GLOBCOVER_SOUTH_LAT_LIMIT = -65.
-GLOBCOVER_WEST_LON_LIMIT = 180.
-GLOBCOVER_EAST_LON_LIMIT = -180.
+GLOBCOVER_WEST_LON_LIMIT = -180.
+GLOBCOVER_EAST_LON_LIMIT = 180.
 
-COMPRESSION = "gzip"
+CROP_WEST_LON = 15.
+CROP_EAST_LON = 35.
+CROP_NORTH_LAT = 72.
+CROP_SOUTH_LAT = 56.
+
+HDF5_COMPRESSION = "gzip"
 
 
 def download_gc():
@@ -42,6 +47,26 @@ def unzip(fname):
     print("Extracted files: %s" % ', '.join(fnames))
 
     return fnames
+
+
+def crop_data(fname):
+    """"Crop GLOBCOVER data"""
+    # Calculate X and Y pixel locations
+    left = (CROP_WEST_LON - GLOBCOVER_WEST_LON_LIMIT) / GLOBCOVER_RESOLUTION
+    right = (CROP_EAST_LON - GLOBCOVER_WEST_LON_LIMIT) / GLOBCOVER_RESOLUTION
+    top = (GLOBCOVER_NORTH_LAT_LIMIT - CROP_NORTH_LAT) / GLOBCOVER_RESOLUTION
+    low = (GLOBCOVER_NORTH_LAT_LIMIT - CROP_SOUTH_LAT) / GLOBCOVER_RESOLUTION
+
+    # Output filename
+    fname_out = os.path.join(os.path.dirname(os.path.abspath(fname)),
+                             "crop_" + os.path.basename(fname))
+
+    # Call gdal_translate
+    cmd = "gdal_translate -srcwin %.0f %.0f %.0f %.0f %s %s" % \
+        (left, top, right - left, low - top, fname, fname_out)
+    os.system(cmd)
+
+    return fname_out
 
 
 def read_tif(fname):
@@ -70,12 +95,8 @@ def read_legend(fname):
 def calc_lonlats(shape):
     """Calculate longitude and latitude vectors."""
     lat_shape, lon_shape = shape
-    lons = np.linspace(GLOBCOVER_WEST_LON_LIMIT - GLOBCOVER_RESOLUTION / 2.,
-                       GLOBCOVER_EAST_LON_LIMIT + GLOBCOVER_RESOLUTION / 2.,
-                       lon_shape)
-    lats = np.linspace(GLOBCOVER_NORTH_LAT_LIMIT - GLOBCOVER_RESOLUTION / 2.,
-                       GLOBCOVER_SOUTH_LAT_LIMIT + GLOBCOVER_RESOLUTION / 2.,
-                       lat_shape)
+    lons = np.linspace(CROP_WEST_LON, CROP_EAST_LON, lon_shape)
+    lats = np.linspace(CROP_NORTH_LAT, CROP_SOUTH_LAT, lat_shape)
 
     return lons, lats
 
@@ -84,7 +105,7 @@ def save_to_hdf5(fname, data, lons, lats, legend):
     """Save data and data legend to HDF5 file."""
     print("Writing data to %s" % fname)
     with h5py.File(fname, 'w') as fid:
-        fid.create_dataset('data', data=data, compression=COMPRESSION)
+        fid.create_dataset('data', data=data, compression=HDF5_COMPRESSION)
         fid['data'].attrs['description'] = "GLOBCOVER v2.3 data"
         fid['longitudes'] = lons
         fid['longitudes'].attrs['description'] = "Center of pixel longitudes"
@@ -107,7 +128,8 @@ def main():
     fnames = unzip(zip_fname)
     for fname in fnames:
         if fname.endswith('.tif') and 'CLA' not in fname:
-            data = read_tif(fname)
+            out_fname = crop_data(fname)
+            data = read_tif(out_fname)
         if fname.endswith('.xls'):
             legend = read_legend(fname)
     lons, lats = calc_lonlats(data.shape)
