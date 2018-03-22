d0a72abc3649a90abdbfb6a4a7d1d5b1206fb7ed
Panu Lahtinen <pnuu+git@iki.fi>
2018-03-22 07:49:46 +0200

    Ignore trollimage when calculating test coverage

--- bin/get_test_coverage.sh
+++ bin/get_test_coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-coverage run --omit=*/python2.7/*,*test_*py,*init*py,*version.py,setup.py,*/satpy/* setup.py test
+coverage run --omit=*/python2.7/*,*test_*py,*init*py,*version.py,setup.py,*/satpy/*,*/trollimage/* setup.py test
 coverage report -m
