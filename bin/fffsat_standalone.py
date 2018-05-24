41ad91997e61a0ec6dcf94c6801e7a16bfdb2925
Panu Lahtinen <pnuu+git@iki.fi>
2018-05-24 15:46:35 +0300

    Create a message object for fff.run()

--- bin/fffsat_standalone.py
+++ bin/fffsat_standalone.py
@@ -13,6 +13,7 @@ import sys
 import logging
 import logging.config
 
+from posttroll.message import Message
 from fffsat.utils import read_config
 from fffsat.forest_fire import ForestFire
 
@@ -20,14 +21,25 @@ from fffsat.forest_fire import ForestFire
 def main():
     config = read_config(sys.argv[1])
     sat_fname = sys.argv[2]
+    sat_tag = config['sat_message_tag']
+    msg_dict = {'platform_name': 'NOAA-19',
+                'sensor': 'avhrr-3',
+                'collection': {sat_tag:
+                               {'dataset': [{'uri': sat_fname}]}}}
     if len(sys.argv) > 3:
         cma_fname = sys.argv[3]
+        cma_tag = config['cma_message_tag']
+        msg_dict['collection'][cma_tag] = {'dataset': [{'uri': cma_fname}]}
     else:
         cma_fname = None
     logging.config.dictConfig(config['standalone_log_config'])
     fff = ForestFire(config)
-    if fff.run(sat_fname=sat_fname, cma_fname=cma_fname):
-        fff.save_text()
+    msg = Message('foo', 'bar', msg_dict)
+    try:
+        if fff.run(msg=msg, sat_fname=sat_fname, cma_fname=cma_fname):
+            fff.save_text()
+    finally:
+        fff._pub.stop()
 
 if __name__ == "__main__":
     main()
